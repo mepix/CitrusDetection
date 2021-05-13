9b709224cf4695af372de1d412a91027c6e601e5
mepix <13284555+mepix@users.noreply.github.com>
2021-05-13 11:39:07 -0700

    Cleaned up Code Base

--- src/orangedetect-xc/main.cpp
+++ src/orangedetect-xc/main.cpp
@@ -5,87 +5,51 @@
 //  Created by Merrick Campbell on 5/12/21.
 //
 
-#include <iostream>
-#include <librealsense2/rs.hpp> // Include RealSense Cross Platform API
-//#include "example.hpp"          // Include short list of convenience functions for rendering
+// Include RealSense Cross Platform API
+#include <librealsense2/rs.hpp>
+
+// Include OpenCV API
+#include <opencv2/opencv.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+
+// Other Includes
 #include <chrono>
-
-//#include <imgui.h>
-//#include "imgui_impl_glfw.h"
-
-// Includes for time display
 #include <sstream>
 #include <iostream>
 #include <iomanip>
 
-#include <opencv2/opencv.hpp>   // Include OpenCV API
-#include <opencv2/imgproc/imgproc.hpp>
-
+// Project Classes
 #include "rgbd.hpp"
 
+//TODO: Move These to input arguments before making the repo public
 bool useLiveStream = false;
 std::string pathToBag = "/Users/merrickcampbell/Movies/realsense-bags/orange_groves/orange_1/20210428_091503.bag";
-
 
 int main(int argc, char * argv[]) try
 {
     std::cout << "Running Citrus Detector" << std::endl;
 
+    // Setup L515 Camera
     CamRGBD citrusCam;
     citrusCam.setPathToBag(pathToBag);
     citrusCam.initStream(useLiveStream);
-    
-//    // Declare depth colorizer for pretty visualization of depth data
-//    rs2::colorizer color_map;
-//
-//    // Create a Pipeline - this serves as a top-level API for streaming and processing frames
-//    rs2::pipeline pipe;
-//
-//    // Configure and start the pipeline
-//    if (useLiveStream) {
-//        pipe.start();
-//
-//    } else { // Load From Bag
-//        rs2::config cfg;
-//        cfg.enable_device_from_file(pathToBag);
-//        cfg.enable_stream(RS2_STREAM_DEPTH);
-//        cfg.enable_stream(RS2_STREAM_COLOR);
-//        pipe.start(cfg); //File will be opened in read mode at this point
-//    }
-//
-//    // RealSense Frame Alignment Tool
-//    rs2::align alignFrames(RS2_STREAM_DEPTH);
-////    rs2::align alignFrames(RS2_STREAM_COLOR);
-    
-    //using namespace cv;
+        
+    // Create Windows for Visualization
     const auto windowDepth = "Depth Image";
     const auto windowColor = "Color Image";
     cv::namedWindow(windowDepth, cv::WINDOW_AUTOSIZE);
     cv::namedWindow(windowColor, cv::WINDOW_AUTOSIZE);
     
-    //while(true)
-    while (cv::waitKey(5) < 0) // && getWindowProperty(window_name, cv::WND_PROP_AUTOSIZE) >= 0)
+    while (cv::waitKey(5) < 0)
     {
+        // Get Current Frames
         citrusCam.procPipe();
         cv::Mat imageDepth = citrusCam.getFrame(CamRGBD::frameType::RGBD_DEPTH);
         cv::Mat imageColor = citrusCam.getFrame(CamRGBD::frameType::RGBD_COLOR);
         
-//        rs2::frameset data = pipe.wait_for_frames(); // Wait for next set of frames from the camera
-//        // Align all frames to depth viewport
-//        data = alignFrames.process(data);
-//        rs2::frame depth = data.get_depth_frame().apply_filter(color_map);
-//        rs2::frame color = data.get_color_frame();
-//
-//        // Query frame size (width and height)
-//        const int w = depth.as<rs2::video_frame>().get_width();
-//        const int h = depth.as<rs2::video_frame>().get_height();
-//
-//        // Create OpenCV matrix of size (w,h) from the colorized depth data
-//        cv::Mat imageDepth(cv::Size(w, h), CV_8UC3, (void*)depth.get_data(), cv::Mat::AUTO_STEP);
-//        cv::Mat imageColor(cv::Size(w, h), CV_8UC3, (void*)color.get_data(), cv::Mat::AUTO_STEP);
-//        cv::cvtColor(imageColor, imageColor, cv::COLOR_RGB2BGR);
+
         
-        // Update the window with new data
+        // Visualize
         cv::imshow(windowDepth, imageDepth);
         cv::imshow(windowColor, imageColor);
     }
